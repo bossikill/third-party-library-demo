b8afce54eb550d042c00ee5d1a2d20c8cbfab3bf
zhengshilong <550476877@qq.com>
2024-08-20 16:29:58 +0800

    feat: 新增没有持久化的zustand

--- src/views/zustand/render-issue/store/useConfigStore.ts
+++ src/views/zustand/render-issue/store/useConfigStore.ts
@@ -1,6 +1,8 @@
-import { createJSONStorage, devtools, persist } from 'zustand/middleware'
+import { devtools } from 'zustand/middleware'
 import { shallow } from 'zustand/shallow'
 import { createWithEqualityFn } from 'zustand/traditional'
+
+// 没有持久化的版本
 
 interface IStore {
   theme: string
@@ -10,24 +12,11 @@ interface IStore {
 }
 
 export const useConfigStore = createWithEqualityFn<IStore>()(
-  persist(
-    devtools(set => ({
-      theme: 'light',
-      lang: 'zh-CN',
-      setLang: (lang: string) => set({ lang }, false, 'setLang'),
-      setTheme: (theme: string) => set({ theme }, false, 'setTheme')
-    })),
-    {
-      // name of item in the storage (must be unique)
-      name: 'third-party-library-demo_render-issue',
-      // (optional) by default the 'localStorage' is used
-      storage: createJSONStorage(() => localStorage)
-      // 选择持久化的数据
-      // partialize: state => ({ checkedMaterial: state.checkedMaterial })
-      // 排除不需要持久化的数据
-      // partialize: state =>
-      //   Object.fromEntries(Object.entries(state).filter(([key]) => !['queueInGenIds', 'queueGenUrl'].includes(key)))
-    }
-  ),
+  devtools(set => ({
+    theme: 'light',
+    lang: 'zh-CN',
+    setLang: (lang: string) => set({ lang }, false, 'setLang'),
+    setTheme: (theme: string) => set({ theme }, false, 'setTheme')
+  })),
   shallow
 )
---
+++ src/views/zustand/render-issue/store/useConfigStore_v2.ts
@@ -0,0 +1,35 @@
+import { createJSONStorage, devtools, persist } from 'zustand/middleware'
+import { shallow } from 'zustand/shallow'
+import { createWithEqualityFn } from 'zustand/traditional'
+
+// 有持久化的版本
+
+interface IStore {
+  theme: string
+  lang: string
+  setTheme: (theme: string) => void
+  setLang: (lang: string) => void
+}
+
+export const useConfigStore = createWithEqualityFn<IStore>()(
+  persist(
+    devtools(set => ({
+      theme: 'light',
+      lang: 'zh-CN',
+      setLang: (lang: string) => set({ lang }, false, 'setLang'),
+      setTheme: (theme: string) => set({ theme }, false, 'setTheme')
+    })),
+    {
+      // name of item in the storage (must be unique)
+      name: 'third-party-library-demo_render-issue',
+      // (optional) by default the 'localStorage' is used
+      storage: createJSONStorage(() => localStorage)
+      // 选择持久化的数据
+      // partialize: state => ({ checkedMaterial: state.checkedMaterial })
+      // 排除不需要持久化的数据
+      // partialize: state =>
+      //   Object.fromEntries(Object.entries(state).filter(([key]) => !['queueInGenIds', 'queueGenUrl'].includes(key)))
+    }
+  ),
+  shallow
+)
