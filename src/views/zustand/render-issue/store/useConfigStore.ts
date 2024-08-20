747be6ab9cc191e7ad84ef6a53858ac9ac68db4f
zhengshilong <550476877@qq.com>
2024-08-20 15:36:34 +0800

    fix: zustand配置错误的问题

--- src/views/zustand/render-issue/store/useConfigStore.ts
+++ src/views/zustand/render-issue/store/useConfigStore.ts
@@ -11,26 +11,23 @@ interface IStore {
 
 export const useConfigStore = createWithEqualityFn<IStore>()(
   persist(
-    devtools(
-      set => ({
-        theme: 'light',
-        lang: 'zh-CN',
-        setLang: (lang: string) => set({ lang }, false, 'setLang'),
-        setTheme: (theme: string) => set({ theme }, false, 'setTheme')
-      }),
-      {
-        // name of item in the storage (must be unique)
-        name: 'third-party-library-demo_render-issue',
-        // (optional) by default the 'localStorage' is used
-        storage: createJSONStorage(() => sessionStorage)
-        // 选择持久化的数据
-        // partialize: state => ({ checkedMaterial: state.checkedMaterial })
-        // 排除不需要持久化的数据
-        // partialize: state =>
-        //   Object.fromEntries(Object.entries(state).filter(([key]) => !['queueInGenIds', 'queueGenUrl'].includes(key)))
-      }
-    ),
-    { name: 'config' }
+    devtools(set => ({
+      theme: 'light',
+      lang: 'zh-CN',
+      setLang: (lang: string) => set({ lang }, false, 'setLang'),
+      setTheme: (theme: string) => set({ theme }, false, 'setTheme')
+    })),
+    {
+      // name of item in the storage (must be unique)
+      name: 'third-party-library-demo_render-issue',
+      // (optional) by default the 'localStorage' is used
+      storage: createJSONStorage(() => localStorage)
+      // 选择持久化的数据
+      // partialize: state => ({ checkedMaterial: state.checkedMaterial })
+      // 排除不需要持久化的数据
+      // partialize: state =>
+      //   Object.fromEntries(Object.entries(state).filter(([key]) => !['queueInGenIds', 'queueGenUrl'].includes(key)))
+    }
   ),
   shallow
 )
